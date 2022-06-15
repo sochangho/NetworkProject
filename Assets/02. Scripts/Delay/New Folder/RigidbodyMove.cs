359cee3ef0935a24af315add02b14861dc48e5f1
pix1212 <101629336+pix1212@users.noreply.github.com>
2022-06-15 21:49:01 +0900

    feat : 플레이어 이동

---
+++ Assets/02. Scripts/Delay/New Folder/RigidbodyMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidbodyMove : MonoBehaviour
+{
+    Rigidbody rb;
+    float speed = 1f;
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float xMove = Input.GetAxis("Horizontal");
+        float zMove = Input.GetAxis("Vertical");
+
+        Vector3 getVel = new Vector3(xMove, 0, zMove) * speed;
+        rb.velocity = getVel;
+        rb.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(getVel), 0.15f);
+    }
+}
---
+++ Assets/02. Scripts/Delay/PlayerMoving.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoving : MonoBehaviour
+{
+    public float speed = 20f;
+    private Vector3 movement;
+
+    private Animator anim;
+    private Rigidbody modelRigidBody;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        modelRigidBody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        movement = new Vector3(x, 0.0f, z).normalized * speed * Time.deltaTime;
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (movement.magnitude < 0.01f)
+        {
+            anim.SetBool("isRun", false);
+            return;
+        }
+
+        modelRigidBody.velocity = movement;
+        modelRigidBody.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
+        anim.SetBool("isRun", true);
+    }
+
+
+}
+
