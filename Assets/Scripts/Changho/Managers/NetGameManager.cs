52931739a77ffb43bebc786ac90c144b69c102c5
sochangho <ckdgh940@naver.com>
2022-06-20 21:45:52 +0900

    캐릭터 생성

---
+++ Assets/Scripts/Changho/Managers/NetGameManager.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Photon.Realtime;
+using Photon.Pun;
+using Photon.Pun.UtilityScripts;
+
+namespace Changho.Managers
+{
+    public class NetGameManager : GamePhotonManager<NetGameManager>
+    {
+
+        public List<Transform> characterSpwanList;
+
+        private void Start()
+        {
+
+            GameOwnPlayerInit();
+        }
+
+
+
+        #region PhotonCallBacks
+
+        public override void OnPlayerPropertiesUpdate(Player targetPlayer, ExitGames.Client.Photon.Hashtable changedProps)
+        {
+            if (changedProps.ContainsKey(GameData.PLAYER_LOAD))
+            {
+                if (CheckAllPlayerLoadLevel())
+                {
+                    //게임시작
+                    CreateCharacters();
+                }
+                else
+                {
+                    
+                }
+            }
+        }
+
+        #endregion
+
+
+
+        public void GameOwnPlayerInit()
+        {
+
+           var localProps = PhotonNetwork.LocalPlayer.CustomProperties;
+           ExitGames.Client.Photon.Hashtable props = new ExitGames.Client.Photon.Hashtable();
+
+            props.Add(ConfigData.CHARACTER, localProps[ConfigData.CHARACTER]);
+            props.Add(ConfigData.LOAD, true);
+
+        }
+
+
+        private void CreateCharacters()
+        {
+
+          string playerName =  PlayerLoad(PhotonNetwork.LocalPlayer);
+
+            if(playerName == null)
+            {
+                Debug.LogError("이름을 찾을 수 없음");
+                return;
+            }
+
+            int number = PhotonNetwork.LocalPlayer.GetPlayerNumber();
+            PhotonNetwork.Instantiate(playerName, characterSpwanList[number].position, characterSpwanList[number].rotation);
+
+
+        }
+
+
+        private bool CheckAllPlayerLoadLevel()
+        {
+            return CountPlayer() == PhotonNetwork.PlayerList.Length;
+        }
+
+        private int CountPlayer()
+        {
+            int count = 0;
+            for(int i = 0; i < PhotonNetwork.PlayerList.Length; i++)
+            {
+                object value; 
+                if(PhotonNetwork.PlayerList[i].CustomProperties.TryGetValue(ConfigData.LOAD , out value))
+                {
+                    if ((bool)value)
+                    {
+                        count++;
+                    }
+                }
+            }
+
+
+            return count;
+        }
+
+
+        private string PlayerLoad(Player player)
+        {
+
+            string playerName = null;
+
+            object type;
+           if( player.CustomProperties.TryGetValue(ConfigData.CHARACTER , out type))
+            {
+
+                Changho.Data.CharaterInfo[] charaterInfos = Resources.LoadAll<Changho.Data.CharaterInfo>("Changho/Prefaps/Characters");
+
+                foreach(var characterinfo in charaterInfos)
+                {
+
+                    if((CharacterType)type == characterinfo.characterType)
+                    {
+                        playerName = characterinfo.gameObject.name;
+                        break;
+                    }
+
+                }
+
+
+              
+            }
+            
+
+            return playerName;
+        }
+
+    }
+}
