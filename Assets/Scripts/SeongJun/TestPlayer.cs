b6febfd02feaec308e37dd3a224ae8d7b0fe3ae1
longbo55 <wnsdl9703@gmail.com>
2022-06-21 16:58:45 +0900

    220621SeoungJun
    채팅시스템 기능 완성 (디테일한 조정 필요)

--- Assets/Scripts/SeongJun/TestPlayer.cs
+++ Assets/Scripts/SeongJun/TestPlayer.cs
@@ -10,7 +10,7 @@ namespace SeongJun {
         //플레이어 이름 띄우는건 완성 랭크 시스템도 지금은 숫자를 움직이는걸로 했지만 최종적으론 위치값을 움직이게 할거 
         public int kill;
         public int ranking;
-        public Text playerText;
+        public Text playerNameText;
         public Text killText;
         public void Start()
         {
@@ -48,15 +48,42 @@ namespace SeongJun {
         {
             kill++;
             killText.text = kill.ToString();
-            gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector3(0, -30 * (ranking + 1), 0);
+            for (int i = 0; i < KillManager.Instance.playerList.Count; i++)
+            {
+                if (kill > KillManager.Instance.playerList[i].gameObject.GetComponent<TestPlayer>().kill
+                   && ranking > KillManager.Instance.playerList[i].gameObject.GetComponent<TestPlayer>().ranking)
+                {
+                    ranking--;
+                    StopCoroutine(MoveUP(i));
+                    StartCoroutine(MoveUP(i));
+                    KillManager.Instance.playerList[i].gameObject.GetComponent<TestPlayer>().ranking++;
+                }
+            }
+            for (int i = 0; i < KillManager.Instance.playerList.Count; i++)
+            {
+                KillManager.Instance.playerList[i].gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector3(0, -30 * (KillManager.Instance.playerList[i].gameObject.GetComponent<TestPlayer>().ranking + 1), 0);
+            }
         }
-        IEnumerator move() {
+        //순위가 상승할때 호출하는 MoveUP함수와 그 반대인 MoveDown함수를 따로 만드는게 어떨까?
+        IEnumerator MoveUP(int playerListNumber)
+        {
+            float speed=3f;
+            /*자신의 위치와 목표위치의 차이가 0.1보다 작으면 종료*/
+            while (0.1f > Vector3.Distance(transform.position, new Vector3(0, -30 * (KillManager.Instance.playerList[playerListNumber].gameObject.GetComponent<TestPlayer>().ranking + 1))))
+            {
+                //순서를 가장 앞으로 설정
+                transform.SetAsFirstSibling();
+                //시작하면 점점 커지다가 목표위치의 반정도 오면 원래대로 돌아가는건 어떨까.
+                //자신의 위치를 목표위치로 이동하되 점점 감속함.
+                yield return new WaitForSeconds(0.01f);
+            }
+            //반복문이 종료되면 자신의 위치를 목표위치로 설정
+            KillManager.Instance.playerList[playerListNumber].gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector3(0, -30 * (KillManager.Instance.playerList[playerListNumber].gameObject.GetComponent<TestPlayer>().ranking + 1), 0);
 
-            yield return new WaitForSeconds(0.01f);
         }
         public void SetNickName(string name,int kill)
         {
-            playerText.text = name;
+            playerNameText.text = name;
             killText.text = kill.ToString();
             KillManager.Instance.playerList.Add(gameObject);
         }
