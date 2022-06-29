6a31314c7329d7b61e468587555ccb684d72f285
pix1212 <101629336+pix1212@users.noreply.github.com>
2022-06-29 21:50:37 +0900

    fix: loading ui 작동 수정

--- Assets/LoadingScenesTest/LoadingSceneController.cs
+++ Assets/LoadingScenesTest/LoadingSceneController.cs
@@ -3,49 +3,92 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System;
 
 public class LoadingSceneController : MonoBehaviour
 {
-    static string nextScene;
-
-    [SerializeField]
-    Image progressBar;
-
-    public static void LoadScene(string sceneName)
+    private static LoadingSceneController instance;
+    public static LoadingSceneController Instance
     {
-        nextScene = sceneName;
-        SceneManager.LoadScene("LoadingScene");
+        get
+        {
+            if(instance == null)
+            {
+                var obj = FindObjectOfType<LoadingSceneController>();
+                if(obj != null)
+                {
+                    instance = obj;
+                }
+                else
+                {
+                    instance = Create();
+                }
+            }
+            return instance;
+        }
     }
 
-    void Start()
+    private static LoadingSceneController Create()
     {
+        return Instantiate(Resources.Load<LoadingSceneController>("Loading"));
+    }
+
+    private void Awake()
+    {
+        if(Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    [SerializeField]
+    private CanvasGroup canvasGroup;
+
+    [SerializeField]
+    private Image progressBar;
+
+    private string loadSceneName;
+
+    public void LoadScene(string sceneName)
+    {
+        gameObject.SetActive(true);
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        loadSceneName = sceneName;
         StartCoroutine(LoadSceneProcess());
     }
 
-    IEnumerator LoadSceneProcess()
+    private void OnSceneLoaded(Scene arg0, LoadSceneMode arg1)
     {
-        AsyncOperation op = SceneManager.LoadSceneAsync(nextScene);
+     if(arg0.name == loadSceneName)
+     {
+        StartCoroutine(Fade(false));
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+     }
+    }
+
+    private IEnumerator LoadSceneProcess()
+    {
+        progressBar.fillAmount = 0f;
+        yield return StartCoroutine(Fade(true));
+
+        AsyncOperation op = SceneManager.LoadSceneAsync(loadSceneName);
         op.allowSceneActivation = false;
 
         float timer = 0f;
-
-        while (!op.isDone)
+        while(!op.isDone)
         {
-            InfiniteLoopDetector.Run();
-
             yield return null;
-
             if(op.progress < 0.9f)
             {
-                progressBar.fillAmount = op.progress;                
+                progressBar.fillAmount = op.progress;
             }
-            else
-            {
+            else{
                 timer += Time.unscaledDeltaTime;
-                progressBar.fillAmount = Mathf.Lerp(0.9f, 1.0f, timer);
-
-                if(progressBar.fillAmount >= 1.0f)
-                {                    
+                progressBar.fillAmount = Mathf.Lerp(0.9f, 1f, timer);
+                if(progressBar.fillAmount >= 1f)
+                {
                     op.allowSceneActivation = true;
                     yield break;
                 }
@@ -53,5 +96,19 @@ public class LoadingSceneController : MonoBehaviour
         }
     }
 
+    private IEnumerator Fade(bool isFadeIn)
+    {
+        float timer = 0f;
+        while(timer <= 1f)
+        {
+            yield return null;
+            timer += Time.unscaledDeltaTime * 3f;
+            canvasGroup.alpha = isFadeIn ? Mathf.Lerp(0f, 1f, timer):Mathf.Lerp(1f, 0f, timer);
+        }
 
+        if(!isFadeIn)
+        {
+            gameObject.SetActive(false);
+        }
+    }
 }
